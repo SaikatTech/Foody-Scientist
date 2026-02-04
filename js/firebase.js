555b0785678d85a358d6e65e878f6712774a46c2
Saikat Chowdhury <84635995+SaikatTech@users.noreply.github.com>
2026-02-04 11:13:20 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -23,4 +23,26 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
 
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqU2zg5IBfvcV6lyMNBU-taYEGdIiOElU",
+  authDomain: "fsonileordering.firebaseapp.com",
+  projectId: "fsonileordering",
+  databaseURL: "https://fsonileordering-default-rtdb.firebaseio.com/",
+  storageBucket: "fsonileordering.firebasestorage.app",
+  messagingSenderId: "570209320858",
+  appId: "1:570209320858:web:2c1c8306bc12f10d520423",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+
+
+
 
