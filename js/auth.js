b45ad53fcbdd09812233778250a3a7cc4b02abd0
Saikat Chowdhury <84635995+SaikatTech@users.noreply.github.com>
2026-02-04 11:23:43 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,5 @@
 // js/auth.js
 import { auth } from "./firebase.js";
-const BASE_PATH = "/Foody-Scientist/";
-
 import {
   signInWithPhoneNumber,
   RecaptchaVerifier,
@@ -10,29 +8,38 @@ import {
 
 import { saveLoginTime, isSessionValid, clearSession } from "./session.js";
 
-window.recaptchaVerifier = new RecaptchaVerifier(
-  auth,
-  "recaptcha-container",
-  { size: "invisible" }
-);
+let recaptchaVerifier = null;
 
-export function sendOTP(phone) {
-  return signInWithPhoneNumber(auth, phone, window.recaptchaVerifier);
+// ✅ Create reCAPTCHA ONLY when needed
+function getRecaptcha() {
+  if (!recaptchaVerifier) {
+    recaptchaVerifier = new RecaptchaVerifier(
+      auth,
+      "recaptcha-container",
+      {
+        size: "invisible"
+      }
+    );
+  }
+  return recaptchaVerifier;
+}
+
+export async function sendOTP(phone) {
+  const verifier = getRecaptcha();
+  return signInWithPhoneNumber(auth, phone, verifier);
 }
 
 export function monitorAuth(redirectIfLoggedIn = false) {
   onAuthStateChanged(auth, (user) => {
     if (user && isSessionValid()) {
       if (redirectIfLoggedIn) {
-        window.location.replace(BASE_PATH + "index.html");
+        window.location.replace("/Foody-Scientist/index.html");
       }
     } else {
       clearSession();
       if (!redirectIfLoggedIn) {
-        window.location.replace(BASE_PATH + "login.html");
+        window.location.replace("/Foody-Scientist/login.html");
       }
     }
   });
 }
-
-
